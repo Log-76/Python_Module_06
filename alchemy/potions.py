9e22911f3bd40970b6ba7938155df879b52d00f7
Logann Leriche <lleriche@c1r6p13.42lehavre.fr>
2026-02-13 17:25:16 +0100

    module6

--- alchemy/potions.py
+++ alchemy/potions.py
@@ -1,14 +1,21 @@
+from alchemy.elements import (create_fire, create_air,
+                              create_earth, create_water)
+
+
 def healing_potion() -> str:
-    return ("Healing potion brewed with [fire_result] and [water_result]")
+    return (f"Healing potion brewed with {create_fire()} and {create_water()}")
 
 
 def strength_potion() -> str:
-    return ("Strength potion brewed with [earth_result] and [fire_result]")
+    return (f"Strength potion brewed with {create_earth()} "
+            f"and {create_fire()}")
 
 
 def invisibility_potion() -> str:
-    return ("Invisibility potion brewed with [air_result] and [water_result]")
+    return (f"Invisibility potion brewed with {create_air()} "
+            f"and {create_water()}")
 
 
 def wisdom_potion() -> str:
-    return ("Wisdom potion brewed with all elements: [all_four_results]")
+    return (f"Wisdom potion brewed with all elements: {create_fire()}"
+            f"{create_air()}{create_earth()}{create_water()}")
